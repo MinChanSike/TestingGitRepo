d4d94a375d6b09bdb851f02c2fb16b132e1f6432
ThuKha <thukha@nete2asia.com>
2022-06-03 10:58:26 +0800

    Change 10:58 text

--- src/ClassLibrary1/Class1.cs
+++ src/ClassLibrary1/Class1.cs
@@ -2,7 +2,7 @@
 
 namespace ClassLibrary1 {
     public class Class1 {
-        public string Text { get; set; } = "This is thu commit";
+        public string Text { get; set; } = "This is thu commit at 10:58am";
         public string Title { get; set; } = "To add another text";
     }
 }

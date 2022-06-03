dd8d1d1d028945537c1da76e0fbc9b46e7b58964
ThuKha <thukha@nete2asia.com>
2022-06-03 10:54:02 +0800

    Add solution and proj

---
+++ src/ClassLibrary1/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ClassLibrary1 {
+    public class Class1 {
+        public string Text { get; set; } = "This is thu commit";
+        public string Title { get; set; } = "To add another text";
+    }
+}
